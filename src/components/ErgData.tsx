d3f77e82549fd53fc37fa01f129d48b7770f3525
Mandi <mandi018@gmail.com>
2024-06-23 16:10:34 -0500

    Display a simple 'no data' string when a month lacks data for an erg machine that the user owns but did not use that month.

--- src/components/ErgData.tsx
+++ src/components/ErgData.tsx
@@ -2,6 +2,7 @@ import {BestDataForErgIF} from "../types/types.ts";
 import DisplayBestStroke from "./DisplayBestStroke.tsx";
 import DisplayBestDistance from "./DisplayBestDistance.tsx";
 import DisplayBestPace from "./DisplayBestPace.tsx";
+import React from "react";
 
 interface ErgDataIF {
   label: string,
@@ -13,11 +14,14 @@ export const ErgData: React.FC<ErgDataIF> = ({label, data, distanceUnits = '500'
   return <>
     {data && <>
         <strong className={`erg-type-label ${label}-label`}>{label}</strong>
-        <ul>
-            <li><DisplayBestPace data={data.bestPace} distanceUnits={distanceUnits}/></li>
-            <li><DisplayBestDistance data={data.bestDistance}/></li>
-            <li><DisplayBestStroke data={data.bestStroke} strokeUnits={strokeUnits}/></li>
-        </ul>
+        {data.bestPace.value === '999:00.0' && <div className="no-data-div">No data</div>}
+        {data.bestPace.value !== '999:00.0' &&
+          <ul>
+              <li><DisplayBestPace data={data.bestPace} distanceUnits={distanceUnits}/></li>
+              <li><DisplayBestDistance data={data.bestDistance}/></li>
+              <li><DisplayBestStroke data={data.bestStroke} strokeUnits={strokeUnits}/></li>
+          </ul>
+        }
     </>
     }
   </>
