8efad2a310be2f60cd54abc7ec74ae23a308ba98
Mandi Burley <mandi018@gmail.com>
2025-06-16 19:39:33 -0500

    Using css vars for erg type colors, updated colors

--- src/consts/consts.ts
+++ src/consts/consts.ts
@@ -1,5 +1,5 @@
 export const ROW_ERG_COLOR = "#afbd22";
-export const BIKE_ERG_COLOR = "#7f99b2";
+export const BIKE_ERG_COLOR = "#9dc7f5";
 export const SKI_ERG_COLOR = "#ffffff";
 
 export const englishMonths = [
